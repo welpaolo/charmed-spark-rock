57359d0501411c98aca12b4eb41d23d668fd1056
Paolo Sottovia <paolo.sottovia@canonical.com>
2024-06-20 05:04:20 +0000

    Add microk8s config

--- tests/integration/config-microk8s-gpu.sh
+++ tests/integration/config-microk8s-gpu.sh
@@ -1,6 +1,18 @@
-microk8s status --wait-ready
-microk8s config | tee ~/.kube/config
-microk8s.enable dns
-microk8s.enable rbac
-microk8s.enable minio
-microk8s.enable gpu
+#!/bin/bash
+
+# This needs to be a separate file as newgrp will not have effect from setup_environment.sh
+echo "Using addons: $MICROK8S_ADDONS"
+sudo microk8s enable storage $MICROK8S_ADDONS
+sudo microk8s kubectl rollout status deployment/hostpath-provisioner -n kube-system
+
+# Wait for gpu operator components to be ready
+while ! sudo microk8s.kubectl logs -n gpu-operator-resources -l app=nvidia-operator-validator | grep "all validations are successful"
+do
+  echo "waiting for validations"
+  sleep 5
+done
+
+# Setup config
+sudo microk8s config > ~/.kube/config
+sudo chown ubuntu:ubuntu ~/.kube/config
+sudo chmod 600 ~/.kube/config
