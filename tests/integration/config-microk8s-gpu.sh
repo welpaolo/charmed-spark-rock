d0fc276d83438121962e9297e7a05ddeb501361f
Paolo Sottovia <paolo.sottovia@canonical.com>
2024-06-20 10:47:37 +0000

    debug space

--- tests/integration/config-microk8s-gpu.sh
+++ tests/integration/config-microk8s-gpu.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 # This needs to be a separate file as newgrp will not have effect from setup_environment.sh
+df -h
 echo "Using addons: $MICROK8S_ADDONS"
 sudo microk8s enable storage $MICROK8S_ADDONS
 sudo microk8s kubectl rollout status deployment/hostpath-provisioner -n kube-system
-
+df -h
 # Wait for gpu operator components to be ready
 while ! sudo microk8s.kubectl logs -n gpu-operator-resources -l app=nvidia-operator-validator | grep "all validations are successful"
 do
@@ -14,6 +15,7 @@ do
     sudo microk8s.kubectl logs -n kube-system -l k8s-app=hostpath-provisioner
     sudo microk8s.kubectl describe pod -n gpu-operator-resources nvidia-operator-validator
     sleep 60
+    df -h
     echo "--------------------------------------------------------------------------------------------------------------------"
 done
 
